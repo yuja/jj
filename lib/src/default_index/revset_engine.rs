206acadd49ef89f11c570fd73909bd045b56ec78
Yuya Nishihara <yuya@tcha.org>
2025-01-20 15:28:22 +0900

    revset: remove stale comment about regex pattern

--- lib/src/default_index/revset_engine.rs
+++ lib/src/default_index/revset_engine.rs
@@ -1163,8 +1163,6 @@ fn build_predicate_fn(
         }
         RevsetFilterPredicate::Author(pattern) => {
             let pattern = pattern.clone();
-            // TODO: Make these functions that take a needle to search for accept some
-            // syntax for specifying whether it's a regex.
             box_pure_predicate_fn(move |index, pos| {
                 let entry = index.entry_by_pos(pos);
                 let commit = store.get_commit(&entry.commit_id())?;
