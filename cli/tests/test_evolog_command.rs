00b20e24fe6c23c84eea8a1fdbbad94a4d6d73b7
Martin von Zweigbergk <martinvonz@google.com>
2025-02-24 10:13:43 -0800

    cli: use same per-commit labels for node and non-node templates
    The top-level `immutable` and `conflict` labels we add to commits in
    the node templates are also useful in the text part. For example, we
    may want to color change ids for immutable commits differently (we do
    that at Google). I also added a `mutable` label, to make it easier to
    define colors for mutable commits without having to define the color
    for all commits and then overriding it for immutable commits.

--- cli/tests/test_evolog_command.rs
+++ cli/tests/test_evolog_command.rs
@@ -231,8 +231,8 @@ fn test_evolog_template() {
     ");
     let output = work_dir.run_jj(["evolog", "-r@", "--color=debug"]);
     insta::assert_snapshot!(output, @r"
-    [1m[38;5;2m<<evolog commit node working_copy::@>>[0m  [1m[38;5;13m<<evolog working_copy commit change_id shortest prefix::k>>[38;5;8m<<evolog working_copy commit change_id shortest rest::kmpptxz>>[39m<<evolog working_copy:: >>[38;5;3m<<evolog working_copy commit author email local::test.user>><<evolog working_copy commit author email::@>><<evolog working_copy commit author email domain::example.com>>[39m<<evolog working_copy:: >>[38;5;14m<<evolog working_copy commit committer timestamp local format::2001-02-03 08:05:09>>[39m<<evolog working_copy:: >>[38;5;12m<<evolog working_copy commit commit_id shortest prefix::2>>[38;5;8m<<evolog working_copy commit commit_id shortest rest::b17ac71>>[39m<<evolog working_copy::>>[0m
-       [1m[38;5;10m<<evolog working_copy empty::(empty)>>[39m<<evolog working_copy:: >>[38;5;10m<<evolog working_copy empty description placeholder::(no description set)>>[39m<<evolog working_copy::>>[0m
+    [1m[38;5;2m<<evolog commit node working_copy mutable::@>>[0m  [1m[38;5;13m<<evolog working_copy mutable commit change_id shortest prefix::k>>[38;5;8m<<evolog working_copy mutable commit change_id shortest rest::kmpptxz>>[39m<<evolog working_copy mutable:: >>[38;5;3m<<evolog working_copy mutable commit author email local::test.user>><<evolog working_copy mutable commit author email::@>><<evolog working_copy mutable commit author email domain::example.com>>[39m<<evolog working_copy mutable:: >>[38;5;14m<<evolog working_copy mutable commit committer timestamp local format::2001-02-03 08:05:09>>[39m<<evolog working_copy mutable:: >>[38;5;12m<<evolog working_copy mutable commit commit_id shortest prefix::2>>[38;5;8m<<evolog working_copy mutable commit commit_id shortest rest::b17ac71>>[39m<<evolog working_copy mutable::>>[0m
+       [1m[38;5;10m<<evolog working_copy mutable empty::(empty)>>[39m<<evolog working_copy mutable:: >>[38;5;10m<<evolog working_copy mutable empty description placeholder::(no description set)>>[39m<<evolog working_copy mutable::>>[0m
        [38;5;8m<<evolog separator::-->>[39m<<evolog:: operation >>[38;5;4m<<evolog operation id short::2931515731a6>>[39m<<evolog:: >><<evolog operation description first_line::add workspace 'default'>><<evolog::>>
     [EOF]
     ");
@@ -246,8 +246,8 @@ fn test_evolog_template() {
     ");
     let output = work_dir.run_jj(["evolog", "-rmain@origin", "--color=debug"]);
     insta::assert_snapshot!(output, @r"
-    [1m[38;5;14m<<evolog commit node immutable::◆>>[0m  [1m[38;5;5m<<evolog commit change_id shortest prefix::q>>[0m[38;5;8m<<evolog commit change_id shortest rest::pvuntsm>>[39m<<evolog:: >>[38;5;3m<<evolog commit author email local::test.user>><<evolog commit author email::@>><<evolog commit author email domain::example.com>>[39m<<evolog:: >>[38;5;6m<<evolog commit committer timestamp local format::2001-02-03 08:05:07>>[39m<<evolog:: >>[38;5;5m<<evolog commit bookmarks name::main>><<evolog commit bookmarks::@>><<evolog commit bookmarks remote::origin>>[39m<<evolog:: >>[1m[38;5;4m<<evolog commit commit_id shortest prefix::e>>[0m[38;5;8m<<evolog commit commit_id shortest rest::8849ae1>>[39m<<evolog::>>
-       [38;5;2m<<evolog empty::(empty)>>[39m<<evolog:: >>[38;5;2m<<evolog empty description placeholder::(no description set)>>[39m<<evolog::>>
+    [1m[38;5;14m<<evolog commit node immutable::◆>>[0m  [1m[38;5;5m<<evolog immutable commit change_id shortest prefix::q>>[0m[38;5;8m<<evolog immutable commit change_id shortest rest::pvuntsm>>[39m<<evolog immutable:: >>[38;5;3m<<evolog immutable commit author email local::test.user>><<evolog immutable commit author email::@>><<evolog immutable commit author email domain::example.com>>[39m<<evolog immutable:: >>[38;5;6m<<evolog immutable commit committer timestamp local format::2001-02-03 08:05:07>>[39m<<evolog immutable:: >>[38;5;5m<<evolog immutable commit bookmarks name::main>><<evolog immutable commit bookmarks::@>><<evolog immutable commit bookmarks remote::origin>>[39m<<evolog immutable:: >>[1m[38;5;4m<<evolog immutable commit commit_id shortest prefix::e>>[0m[38;5;8m<<evolog immutable commit commit_id shortest rest::8849ae1>>[39m<<evolog immutable::>>
+       [38;5;2m<<evolog immutable empty::(empty)>>[39m<<evolog immutable:: >>[38;5;2m<<evolog immutable empty description placeholder::(no description set)>>[39m<<evolog immutable::>>
     [EOF]
     ");
 
@@ -259,7 +259,7 @@ fn test_evolog_template() {
     ");
     let output = work_dir.run_jj(["evolog", "-rroot()", "--color=debug"]);
     insta::assert_snapshot!(output, @r"
-    [1m[38;5;14m<<evolog commit node immutable::◆>>[0m  [1m[38;5;5m<<evolog commit change_id shortest prefix::z>>[0m[38;5;8m<<evolog commit change_id shortest rest::zzzzzzz>>[39m<<evolog:: >>[38;5;2m<<evolog root::root()>>[39m<<evolog:: >>[1m[38;5;4m<<evolog commit commit_id shortest prefix::0>>[0m[38;5;8m<<evolog commit commit_id shortest rest::0000000>>[39m<<evolog::>>
+    [1m[38;5;14m<<evolog commit node immutable::◆>>[0m  [1m[38;5;5m<<evolog immutable commit change_id shortest prefix::z>>[0m[38;5;8m<<evolog immutable commit change_id shortest rest::zzzzzzz>>[39m<<evolog immutable:: >>[38;5;2m<<evolog immutable root::root()>>[39m<<evolog immutable:: >>[1m[38;5;4m<<evolog immutable commit commit_id shortest prefix::0>>[0m[38;5;8m<<evolog immutable commit commit_id shortest rest::0000000>>[39m<<evolog immutable::>>
     [EOF]
     ");
 
--- cli/tests/test_log_command.rs
+++ cli/tests/test_log_command.rs
@@ -1193,8 +1193,8 @@ fn test_graph_template_color() {
     ");
     let output = work_dir.run_jj(["--color=debug", "log", "-T", template]);
     insta::assert_snapshot!(output, @r"
-    [1m[38;5;2m<<log commit node working_copy::@>>[0m  [1m[38;5;2m<<log commit working_copy description::single line>>[0m
-    <<log commit node::○>>  [38;5;1m<<log commit description::first line>>[39m
+    [1m[38;5;2m<<log commit node working_copy mutable::@>>[0m  [1m[38;5;2m<<log commit working_copy description::single line>>[0m
+    <<log commit node mutable::○>>  [38;5;1m<<log commit description::first line>>[39m
     │  [38;5;1m<<log commit description::second line>>[39m
     │  [38;5;1m<<log commit description::third line>>[39m
     [1m[38;5;14m<<log commit node immutable::◆>>[0m
