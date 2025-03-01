7b229e6b97f91c1784ef5d4bacf213bbdcbee0e8
pylbrecht <pylbrecht@mailbox.org>
2025-03-01 06:28:52 +0100

    sign: format link in help
    Following the existing conventions for formatting links.

--- cli/src/commands/sign.rs
+++ cli/src/commands/sign.rs
@@ -44,7 +44,10 @@ pub struct SignArgs {
     ///
     /// While that leads to discomfort for users, which sign with hardware
     /// devices, as of now we cannot reliably check if a commit is already
-    /// signed by the user without creating a signature (see https://github.com/jj-vcs/jj/issues/5786).
+    /// signed by the user without creating a signature (see [#5786]).
+    ///
+    /// [#5786]:
+    ///     https://github.com/jj-vcs/jj/issues/5786
     #[arg(
         long, short,
         value_name = "REVSETS",
