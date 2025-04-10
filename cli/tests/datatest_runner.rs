8c4586ab09519c2f632ebc0a53d804a1198bb20f
Jonas Greitemann <jgreitemann@gmail.com>
2025-04-10 15:32:04 +0200

    config-schema: add sample config files to exercise schema tests
    Adds a bunch of additional sample config toml files. Via the
    `datatest_runner`, these each correspond to a test case to check that
    the toml is correctly (in-)validated according to the schema.
    
    The `valid/*.toml` files typically define multiple related config
    options at once. Where there's some overlap with the default configs in
    `cli/src/config`, the aim was to choose different allowed values, e.g.
    hex colors, file size in bytes (numeric), etc.
    
    The `invalid/*.toml` files typically only define a single offending
    property such as to not obscure individual false negatives. All of the
    "invalid" files are still valid toml as the aim is not to test the
    `toml_edit` crate or Taplo.
    
    The sample files all contain a Taplo schema directive. This allows them
    to be validated against the schema on the fly by Taplo's LSP and derived
    IDE plugins to speed up editing and immediately highlight offending
    options.
    
    Closes #5695.

--- cli/tests/datatest_runner.rs
+++ cli/tests/datatest_runner.rs
@@ -2,8 +2,18 @@ mod test_config_schema;
 
 datatest_stable::harness! {
     {
-        test = test_config_schema::taplo_check_config,
+        test = test_config_schema::taplo_check_config_valid,
         root = "src/config",
+        pattern = r".*\.toml",
+    },
+    {
+        test = test_config_schema::taplo_check_config_valid,
+        root = "tests/sample-configs/valid",
+        pattern = r".*\.toml",
+    },
+    {
+        test = test_config_schema::taplo_check_config_invalid,
+        root = "tests/sample-configs/invalid",
         pattern = r".*\.toml",
     }
 }
--- cli/tests/test_config_schema.rs
+++ cli/tests/test_config_schema.rs
@@ -1,10 +1,11 @@
 use std::path::Path;
 use std::process::Command;
+use std::process::Output;
 use std::process::Stdio;
 
 use testutils::ensure_running_outside_ci;
 
-pub(crate) fn taplo_check_config(file: &Path) -> datatest_stable::Result<()> {
+fn taplo_check_config(file: &Path) -> datatest_stable::Result<Option<Output>> {
     if Command::new("taplo")
         .arg("--version")
         .stdout(Stdio::null())
@@ -13,28 +14,42 @@ pub(crate) fn taplo_check_config(file: &Path) -> datatest_stable::Result<()> {
     {
         ensure_running_outside_ci("`taplo` must be in the PATH");
         eprintln!("Skipping test because taplo is not installed on the system");
-        return Ok(());
+        return Ok(None);
     }
 
     // Taplo requires an absolute URL to the schema :/
     let root = std::path::PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-    let taplo_res = Command::new("taplo")
-        .args([
-            "check",
-            "--schema",
-            &format!("file://{}/src/config-schema.json", root.display()),
-        ])
-        .arg(file.as_os_str())
-        .stdout(Stdio::piped())
-        .stderr(Stdio::piped())
-        .spawn()?
-        .wait_with_output()?;
+    Ok(Some(
+        Command::new("taplo")
+            .args([
+                "check",
+                "--schema",
+                &format!("file://{}/src/config-schema.json", root.display()),
+            ])
+            .arg(file.as_os_str())
+            .stdout(Stdio::piped())
+            .stderr(Stdio::piped())
+            .spawn()?
+            .wait_with_output()?,
+    ))
+}
 
-    if !taplo_res.status.success() {
-        eprintln!("Failed to validate {}:", file.display());
-        eprintln!("{}", String::from_utf8_lossy(&taplo_res.stderr));
-        Err("Validation failed".into())
-    } else {
-        Ok(())
+pub(crate) fn taplo_check_config_valid(file: &Path) -> datatest_stable::Result<()> {
+    if let Some(taplo_res) = taplo_check_config(file)? {
+        if !taplo_res.status.success() {
+            eprintln!("Failed to validate {}:", file.display());
+            eprintln!("{}", String::from_utf8_lossy(&taplo_res.stderr));
+            return Err("Validation failed".into());
+        }
     }
+    Ok(())
+}
+
+pub(crate) fn taplo_check_config_invalid(file: &Path) -> datatest_stable::Result<()> {
+    if let Some(taplo_res) = taplo_check_config(file)? {
+        if taplo_res.status.success() {
+            return Err("Validation unexpectedly passed".into());
+        }
+    }
+    Ok(())
 }
