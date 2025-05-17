9d5c146c9bb37f2ca6477679f3d84bb38d20eff2
Vincent Ging Ho Yim <cenviity@gmail.com>
2025-05-17 23:56:35 +1000

    cli describe: remove redundant assertion of conflicting flags
    This assertion is redundant given both the `conflicts_with = "edit"` declaration on the
    `no_edit` flag and the passing `test_edit_cannot_be_used_with_no_edit` test.

--- cli/src/commands/describe.rs
+++ cli/src/commands/describe.rs
@@ -175,9 +175,6 @@ pub(crate) fn cmd_describe(
         })
         .collect_vec();
 
-    // edit and no_edit are conflicting arguments and therefore it should not
-    // be possible for both to be true at the same time.
-    assert!(!(args.edit && args.no_edit));
     let use_editor = args.edit || (shared_description.is_none() && !args.no_edit);
 
     if let Some(trailer_template) = parse_trailers_template(ui, &tx)? {
