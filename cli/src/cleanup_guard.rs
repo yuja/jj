5595bef26c9031f26cb18648a5f93c18a5f7be72
Austin Seipp <aseipp@pobox.com>
2025-06-27 10:20:51 -0500

    cli: add `unsafe` bodies for `cleanup_guard` code
    In Rust 2024, a function declaration marked `unsafe` does NOT implicitly
    make the body unsafe. All these unsafe functions also need to be wrapped
    in safe blocks now, too.
    
    Signed-off-by: Austin Seipp <aseipp@pobox.com>

--- cli/src/cleanup_guard.rs
+++ cli/src/cleanup_guard.rs
@@ -63,33 +63,35 @@ mod platform {
 
     /// Safety: Must be called at most once
     pub unsafe fn init() -> io::Result<()> {
-        let (send, recv) = UnixDatagram::pair()?;
+        unsafe {
+            let (send, recv) = UnixDatagram::pair()?;
 
-        // Spawn a background thread that waits for the signal handler to write a signal
-        // into it
-        thread::spawn(move || {
-            let mut buf = [0];
-            let signal = match recv.recv(&mut buf) {
-                Ok(1) => c_int::from(buf[0]),
-                _ => unreachable!(),
-            };
-            // We must hold the lock for the remainder of the process's lifetime to avoid a
-            // race where a guard is created between `on_signal` and `raise`.
-            let guards = &mut *LIVE_GUARDS.lock().unwrap();
-            if let Err(e) = std::panic::catch_unwind(AssertUnwindSafe(|| on_signal(guards))) {
-                match e.downcast::<String>() {
-                    Ok(s) => eprintln!("signal handler panicked: {s}"),
-                    Err(_) => eprintln!("signal handler panicked"),
+            // Spawn a background thread that waits for the signal handler to write a signal
+            // into it
+            thread::spawn(move || {
+                let mut buf = [0];
+                let signal = match recv.recv(&mut buf) {
+                    Ok(1) => c_int::from(buf[0]),
+                    _ => unreachable!(),
+                };
+                // We must hold the lock for the remainder of the process's lifetime to avoid a
+                // race where a guard is created between `on_signal` and `raise`.
+                let guards = &mut *LIVE_GUARDS.lock().unwrap();
+                if let Err(e) = std::panic::catch_unwind(AssertUnwindSafe(|| on_signal(guards))) {
+                    match e.downcast::<String>() {
+                        Ok(s) => eprintln!("signal handler panicked: {s}"),
+                        Err(_) => eprintln!("signal handler panicked"),
+                    }
                 }
-            }
-            libc::signal(signal, libc::SIG_DFL);
-            libc::raise(signal);
-        });
+                libc::signal(signal, libc::SIG_DFL);
+                libc::raise(signal);
+            });
 
-        SIGNAL_SEND = send.into_raw_fd();
-        libc::signal(SIGINT, handler as libc::sighandler_t);
-        libc::signal(SIGTERM, handler as libc::sighandler_t);
-        Ok(())
+            SIGNAL_SEND = send.into_raw_fd();
+            libc::signal(SIGINT, handler as libc::sighandler_t);
+            libc::signal(SIGTERM, handler as libc::sighandler_t);
+            Ok(())
+        }
     }
 
     // Invoked on a background thread. Process exits after this returns.
@@ -100,15 +102,17 @@ mod platform {
     }
 
     unsafe extern "C" fn handler(signal: c_int) {
-        // Treat the second signal as instantly fatal.
-        static SIGNALED: AtomicBool = AtomicBool::new(false);
-        if SIGNALED.swap(true, Ordering::Relaxed) {
-            libc::signal(signal, libc::SIG_DFL);
-            libc::raise(signal);
-        }
+        unsafe {
+            // Treat the second signal as instantly fatal.
+            static SIGNALED: AtomicBool = AtomicBool::new(false);
+            if SIGNALED.swap(true, Ordering::Relaxed) {
+                libc::signal(signal, libc::SIG_DFL);
+                libc::raise(signal);
+            }
 
-        let buf = [signal as u8];
-        libc::write(SIGNAL_SEND, buf.as_ptr().cast(), buf.len());
+            let buf = [signal as u8];
+            libc::write(SIGNAL_SEND, buf.as_ptr().cast(), buf.len());
+        }
     }
 
     static mut SIGNAL_SEND: RawFd = 0;
